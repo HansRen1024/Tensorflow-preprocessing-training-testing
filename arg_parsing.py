fb9c37d5ec4e00858165d0937233235a2ebf28f5
Hans Ren <171357351@qq.com>
2018-03-21 10:35:16 +0800

    Update parameters

--- arg_parsing.py
+++ arg_parsing.py
@@ -19,43 +19,37 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 4996
 NUM_EXAMPLES_PER_EPOCH_FOR_TEST = 4996
 NUM_LABELS = 17
 IMAGE_SHAPE = 227
-b = tf.Variable(82.4088, dtype=tf.float32, name='b', trainable=False)
-g = tf.Variable(114.4588, dtype=tf.float32, name='g', trainable=False)
-r = tf.Variable(139.1493, dtype=tf.float32, name='r', trainable=False)
-MEAN = [b, g, r]
+mean = [82.4088, 114.4588, 139.1493] # BGR
 
+INITIAL_LEARNING_RATE = 0.01
 MOVING_AVERAGE_DECAY = 0.9999
 NUM_EPOCHS_PER_DECAY = 50
 LEARNING_RATE_DECAY_FACTOR = 0.75
-INITIAL_LEARNING_RATE = 0.01
 STEPS_TO_VAL = 1000
 WEIGHT_DECAY = 2e-4
+b = tf.Variable(mean[0], dtype=tf.float32, name='b', trainable=False)
+g = tf.Variable(mean[1], dtype=tf.float32, name='g', trainable=False)
+r = tf.Variable(mean[2], dtype=tf.float32, name='r', trainable=False)
+MEAN = [b, g, r]
 
+DEBUG = False
+DATASET_DIR = 'data/' # Path to data directory.
+MODEL_DIR = 'models/' # Directory where to write event logs and checkpoint.
+BATCH_SIZE = 16
+LOG_FREQUENCY = 100 # How often to log results to the console.
+MAX_STEPS = 10000 # Number of batches to run.
+LOG_DEVICE_PLACEMENT = False # Whether to log device placement.
+USE_FP16 = False # Train the model using fp16.
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', type=str,
                     help='Either `training` or `testing` or `training_dis` .')
-parser.add_argument('--data_dir', type=str, default='data/',
-                    help='Path to data directory.')
-parser.add_argument('--model_dir', type=str, default='models/',
-                    help='Directory where to write event logs and checkpoint.')
-parser.add_argument('--batch_size', type=int, default=8,
-                    help='Number of images to process in a batch.')
-parser.add_argument('--use_fp16', type=bool, default=False,
-                    help='Train the model using fp16.')
-parser.add_argument('--max_steps', type=int, default=10000,
-                    help='Number of batches to run.')
-parser.add_argument('--log_frequency', type=int, default=100,
-                    help='How often to log results to the console.')
-parser.add_argument('--log_device_placement', type=bool, default=False,
-                    help='Whether to log device placement.')
 
 # For distributed
-parser.add_argument("--ps_hosts", type=str,
-                    help="Comma-separated list of hostname:port pairs")
-parser.add_argument("--worker_hosts", type=str,
-                    help="Comma-separated list of hostname:port pairs")
+PS_HOSTS = '10.100.1.100:2222' # Comma-separated list of hostname:port pairs
+WORKER_HOSTS = '10.100.1.100:2224,10.100.1.101:2225' # Comma-separated list of hostname:port pairs
+
 parser.add_argument("--job_name", type=str,
                     help="One of 'ps', 'worker'")
-parser.add_argument("--task_index", type=int, default=0,
+parser.add_argument("--task_index", type=int,
                     help="Index of task within the job")
