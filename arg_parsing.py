b180f6d167d8309f815f40b231cea821fa4a544b
Hans Ren <171357351@qq.com>
2018-03-26 19:51:31 +0800

    Update arg_parsing.py

--- arg_parsing.py
+++ arg_parsing.py
@@ -14,7 +14,7 @@ import tensorflow as tf
 
 import argparse
 
-NET = 'resnet' # squeezenet or mobilenet or mobilenetv2 or resnet ...
+NET = 'squeezenet' # squeezenet or mobilenet or mobilenetv2 or resnet ...
 RESNET_LAYER_NUM = 50 # 50 or 101 or 152 or 200 or ...
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 27013 # number of training data
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 4996 # number of validation data 
@@ -25,14 +25,15 @@ mean = [82.4088, 114.4588, 139.1493] # BGR mean values
 
 INITIAL_LEARNING_RATE = 0.01
 MOVING_AVERAGE_DECAY = 0.9999
-NUM_EPOCHS_PER_DECAY = 50
-LEARNING_RATE_DECAY_FACTOR = 0.75
+NUM_EPOCHS_PER_DECAY = 10
+LEARNING_RATE_DECAY_FACTOR = 0.5
 STEPS_TO_VAL = 1000
 WEIGHT_DECAY = 2e-4 # used for squeezenet and resnet
 
 DEBUG = False
 DATASET_DIR = 'data/' # Path to data directory.
 MODEL_DIR = 'models/' # Directory where to write event logs and checkpoint.
+STEPS_TO_SAVE_MODEL=1000
 BATCH_SIZE = 4
 LOG_FREQUENCY = 100 # How often to log results to the console.
 MAX_STEPS = 10000 # Number of batches to run. If distributiong, all GPU batches.
@@ -51,10 +52,12 @@ MEAN = [b, g, r]
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', type=str,
                     help='Either `training` or `testing` or `training_dis` .')
+parser.add_argument('--lr', type=int, default=INITIAL_LEARNING_RATE)
+parser.add_argument('--debug', type=bool, default=DEBUG)
 
 # For distributed
-PS_HOSTS = '10.100.1.100:2222' # Comma-separated list of hostname:port pairs
-WORKER_HOSTS = '10.100.1.100:2224,10.100.1.101:2225' # Comma-separated list of hostname:port pairs
+PS_HOSTS = '10.100.3.101:2222' # Comma-separated list of hostname:port pairs
+WORKER_HOSTS = '10.100.3.101:2224,10.100.3.100:2225' # Comma-separated list of hostname:port pairs
 
 parser.add_argument("--job_name", type=str,
                     help="One of 'ps', 'worker'")
